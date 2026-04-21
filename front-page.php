71042d6f7e908f3ade697df466060cba73b1053c
talarekr <talarekr@gmail.com>
2026-04-21 18:01:42 +0200

    Rebuild homepage top area with promo header and hero

--- front-page.php
+++ front-page.php
@@ -6,8 +6,6 @@ if (!defined('ABSPATH')) {
 get_header();
 
 get_template_part('template-parts/home/top-bar');
-get_template_part('template-parts/home/brand-selector');
-get_template_part('template-parts/home/category-mega');
 get_template_part('template-parts/home/store-header');
 get_template_part('template-parts/home/banners');
 get_template_part('template-parts/home/home-title');
