5a7525b7245774dba648dd0fc2e3e07e0d518f1a
talarekr <talarekr@gmail.com>
2026-04-21 19:29:35 +0200

    Uspójnij i odchudź karty produktowe na listingach

--- template-parts/home/popular-products.php
+++ template-parts/home/popular-products.php
@@ -33,14 +33,14 @@
                                 </span>
                             <?php endif; ?>
                             <?php echo $product->get_image('woocommerce_thumbnail'); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-                            <div class="gp-product__sku">Numer części: <strong><?php echo esc_html($product->get_sku() ?: 'BRAK'); ?></strong></div>
-                            <h3 class="gp-product__name"><a href="<?php echo esc_url(get_permalink($product->get_id())); ?>"><?php echo esc_html($product->get_name()); ?></a></h3>
-                            <p class="gp-product__price">
+                            <div class="gp-product__sku product-meta">Numer części: <strong><?php echo esc_html($product->get_sku() ?: 'BRAK'); ?></strong></div>
+                            <h3 class="gp-product__name product-title"><a href="<?php echo esc_url(get_permalink($product->get_id())); ?>"><?php echo esc_html($product->get_name()); ?></a></h3>
+                            <p class="gp-product__price product-price">
                                 <?php if ($product->get_regular_price() && $product->is_on_sale()) : ?><span class="gp-product__promo-label">Cena promocyjna</span><span class="gp-product__old"><?php echo esc_html(wc_price($product->get_regular_price())); ?></span><?php endif; ?>
                                 <span class="<?php echo $product->is_on_sale() ? 'gp-product__current gp-product__current--sale' : 'gp-product__current'; ?>"><?php echo wp_kses_post(wc_price($product->get_price())); ?></span>
                             </p>
-                            <div class="gp-product__delivery">Darmowa dostawa: 23–24 kwi</div>
-                            <div class="gp-product__delivery-note">Jeśli zapłacisz do 14:00</div>
+                            <div class="gp-product__delivery product-shipping">Darmowa dostawa: 23–24 kwi</div>
+                            <div class="gp-product__delivery-note product-shipping-sub">Jeśli zapłacisz do 14:00</div>
                         </article>
                     <?php endforeach; ?>
                 </div>
@@ -57,14 +57,14 @@
                             </button>
                             <?php if (!empty($product['discount'])) : ?><span class="gp-product__badge"><?php echo esc_html($product['discount']); ?></span><?php endif; ?>
                             <img src="<?php echo esc_url($product['image']); ?>" alt="<?php echo esc_attr($product['name']); ?>">
-                            <div class="gp-product__sku">Numer części: <strong><?php echo esc_html($product['sku']); ?></strong></div>
-                            <h3 class="gp-product__name"><a href="#"><?php echo esc_html($product['name']); ?></a></h3>
-                            <p class="gp-product__price">
+                            <div class="gp-product__sku product-meta">Numer części: <strong><?php echo esc_html($product['sku']); ?></strong></div>
+                            <h3 class="gp-product__name product-title"><a href="#"><?php echo esc_html($product['name']); ?></a></h3>
+                            <p class="gp-product__price product-price">
                                 <?php if (!empty($product['old_price'])) : ?><span class="gp-product__promo-label">Cena promocyjna</span><span class="gp-product__old"><?php echo esc_html($product['old_price']); ?></span><?php endif; ?>
                                 <span class="gp-product__current<?php echo !empty($product['old_price']) ? ' gp-product__current--sale' : ''; ?>"><?php echo esc_html($product['price']); ?></span>
                             </p>
-                            <div class="gp-product__delivery">Darmowa dostawa: 23–24 kwi</div>
-                            <div class="gp-product__delivery-note">Jeśli zapłacisz do 14:00</div>
+                            <div class="gp-product__delivery product-shipping">Darmowa dostawa: 23–24 kwi</div>
+                            <div class="gp-product__delivery-note product-shipping-sub">Jeśli zapłacisz do 14:00</div>
                         </article>
                     <?php endforeach; ?>
                 </div>
