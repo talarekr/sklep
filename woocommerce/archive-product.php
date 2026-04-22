9d85f1d48f2f02fffc43eb4d241bf3cfa5b9d858
talarekr <talarekr@gmail.com>
2026-04-22 11:07:16 +0200

    Fix WooCommerce category tree rendering on shop/archive sidebar

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -14,19 +14,31 @@ get_header('shop');
 
         <div class="gp-shop-grid">
             <aside class="gp-shop-sidebar">
-                <h3><?php esc_html_e('Kategorie', 'gp-clone'); ?></h3>
-                <ul>
-                    <?php
-                    wp_list_categories([
-                        'taxonomy' => 'product_cat',
-                        'title_li' => '',
-                        'show_count' => false,
-                    ]);
-                    ?>
-                </ul>
+                <h3 class="gp-shop-sidebar__title"><?php esc_html_e('Kategorie', 'gp-clone'); ?></h3>
+                <?php
+                $current_term_id = 0;
+                $active_path_ids = [];
+
+                if (is_tax('product_cat')) {
+                    $current_term = get_queried_object();
+                    if ($current_term instanceof WP_Term) {
+                        $current_term_id = (int) $current_term->term_id;
+                        $active_path_ids = array_map('intval', array_filter(array_merge(
+                            [$current_term_id],
+                            get_ancestors($current_term_id, 'product_cat', 'taxonomy')
+                        )));
+                    }
+                }
+
+                if (function_exists('gp_get_product_category_tree') && function_exists('gp_render_product_category_tree') && gp_get_product_category_tree(0) !== []) {
+                    gp_render_product_category_tree(0, $current_term_id, $active_path_ids);
+                } else {
+                    echo '<p class="gp-shop-sidebar__empty">' . esc_html__('Brak kategorii do wyświetlenia.', 'gp-clone') . '</p>';
+                }
+                ?>
             </aside>
 
-            <section>
+            <section class="gp-shop-content">
                 <?php if (apply_filters('woocommerce_show_page_title', true)) : ?>
                     <h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
                 <?php endif; ?>
