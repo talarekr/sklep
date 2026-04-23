5aa851f9f75bb53b659839deae50eda4b516254d
talarekr <talarekr@gmail.com>
2026-04-23 23:21:15 +0200

    Adjust homepage sections: remove title, add DPF block, hide category tiles

--- front-page.php
+++ front-page.php
@@ -7,7 +7,6 @@ get_header();
 
 get_template_part('template-parts/home/top-bar');
 get_template_part('template-parts/home/store-header');
-get_template_part('template-parts/home/home-title');
 get_template_part('template-parts/home/popular-products');
 
 get_footer();
